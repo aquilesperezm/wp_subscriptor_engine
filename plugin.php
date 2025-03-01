37ab2004398cd47a9cd15197ed1fd7cc9d552b81
Aquiles Perez Miranda <98031710+aquilesperezm@users.noreply.github.com>
2025-03-01 08:22:34 -0500

    First Commit and Folder Structure

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Plugin Name: WP Suscriptor Engine
+* Plugin URI: https://example.com/plugins/pdev
+* Description: A short description of the plugin.
+* Version: 1.0.0
+* Requires at least: 5.3
+* Requires PHP: 5.6
+* Author: Aquiles Pérez Miranda
+* Author URI: https://example.com
+* License: GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Text Domain: pdev
+* Domain Path: /public/lang
+*/
